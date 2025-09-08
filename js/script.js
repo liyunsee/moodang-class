73d2091d81f52dcaa90a658dde322008d9d1c882
yunseul <seulweetie@naver.com>
2025-09-08 16:10:30 +0900

    최종 완성

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // 비주얼 슬라이더
+  const teamSlider = new Swiper(".team-slider", {
+    // Optional parameters
+    // direction: "vertical",
+    loop: true,
+    slidesPerView: 1,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+});
